40d83fbf605722918c171e8d1917b5c4660a48a9
tarahiro <38371023+tarahiro@users.noreply.github.com>
2024-11-14 17:43:04 +0900

    Initial commit

---
+++ Assets/Script/Core/_Template.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using Cysharp.Threading.Tasks;
+using Tarahiro;
+using UniRx;
+
+namespace FakeProject
+{
+    public class _Template
+    {
+    }
+}
