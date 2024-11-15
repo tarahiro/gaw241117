1caf0d8c4d310dfab3089db51aa4e97a40410dcb
tarahiro <tarahiro64@gmail.com>
2024-11-15 14:19:02 +0900

    裏表表示判定

--- Assets/Script/View/CoinView.cs
+++ Assets/Script/View/CoinView.cs
@@ -15,11 +15,17 @@ namespace gaw241117.View
     {
         [Inject] ITouchView _touchView;
         [SerializeField] Rigidbody _rigidbody;
+        [SerializeField] Transform _normalObject;
 
         bool _isInputAcceptable = false;
+        bool _isCoinMoving = false;
+        bool _isCoinRigidbodyPropertySaved = false;
         const float c_fakeXzDirectionMaxForceLengh = .01f;
         const float c_fakeXyScreenPointMaxLength = 500f;
-        const float c_fakeYDirectionForceLengh = .02f;
+        const float c_fakeYDirectionForceLengh = .03f;
+
+        Vector3 _prevPosition;
+        Quaternion _prevQuaternion;
         public void SetInputAcceptable()
         {
             _isInputAcceptable = true;
@@ -30,15 +36,64 @@ namespace gaw241117.View
         {
             if (_isInputAcceptable)
             {
-                if (_touchView.State == TouchConst.TouchState.End)
+                if (!_isCoinMoving)
                 {
-                    Vector2 dir = _touchView.ScreenPoint - _touchView.BeginScreenPoint;
-                    float xzForceLength = c_fakeXzDirectionMaxForceLengh * Mathf.Min(dir.magnitude / c_fakeXyScreenPointMaxLength, 1f);
-                    float x = xzForceLength * dir.x / dir.magnitude;
-                    float z = xzForceLength * dir.y / dir.magnitude;
-                    _rigidbody.AddForce(new Vector3(x, c_fakeYDirectionForceLengh,z), ForceMode.Impulse);
+                    if (_touchView.State == TouchConst.TouchState.End)
+                    {
+                        Vector2 dir = _touchView.ScreenPoint - _touchView.BeginScreenPoint;
+                        float xzForceLength = c_fakeXzDirectionMaxForceLengh * Mathf.Min(dir.magnitude / c_fakeXyScreenPointMaxLength, 1f);
+                        float x = xzForceLength * dir.x / dir.magnitude;
+                        float z = xzForceLength * dir.y / dir.magnitude;
+                        _rigidbody.AddForce(new Vector3(x, c_fakeYDirectionForceLengh, z), ForceMode.Impulse);
+                        _rigidbody.AddTorque(Vector3.right * Mathf.PI / 4f, ForceMode.Impulse);
+
+                        _isCoinMoving = true;
+                        _isCoinRigidbodyPropertySaved = false;
+                    }
                 }
             }
+        }
+
+        void FixedUpdate()
+        {
+            if (_isCoinMoving)
+            {
+                if (!_isCoinRigidbodyPropertySaved)
+                {
+                    _prevPosition = _rigidbody.position;
+                    _prevQuaternion = _rigidbody.rotation;
+                    _isCoinRigidbodyPropertySaved = true;
+
+                }
+                else
+                {
+                    if (_rigidbody.position == _prevPosition && _rigidbody.rotation == _prevQuaternion)
+                    {
+                        _isCoinMoving = false;
+                        Log.DebugLog(IsHead().ToString());
+                    }
+                    else
+                    {
+                        _prevPosition = _rigidbody.position;
+                        _prevQuaternion = _rigidbody.rotation;
+                    }
+                }
+            }
+        }
+
+        public enum CoinState
+        {
+
+        }
+
+        bool IsHead()
+        {
+            return Normal().y > 0f;
+        }
+
+        Vector3 Normal()
+        {
+           return (_normalObject.position - transform.position).normalized;
         }
 
                 
